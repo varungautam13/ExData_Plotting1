d3b99449f9e7c6cca6754256af49566cc3c6c5a8
varungautam13 <varungautam13@gmail.com>
2015-05-07 15:44:35 +0530

    Project Assignment 1
    Removed extra code

--- plot1.R
+++ plot1.R
@@ -12,26 +12,12 @@ Power_Consp$Date <- as.Date(Power_Consp$Date, format="%d/%m/%Y")
 ## Subsetting Power_Consp dataset for dates 2007-02-01 and 2007-02-02
 Consp_Subset <-Power_Consp[Date >="2007-02-01"& Date <="2007-02-02",]
 
-## removing the Power_Consp object to free up memory 
+## removing the Power_Consp object to free up memory space
 rm(Power_Consp)
 
 ## Plotting a histogram for Global Active Power and saving it as plot1.png
 png("plot1.png", width=480,height=480)
 hist(as.numeric(Consp_Subset$Global_active_power), main="Global Active Power", xlab="Global Active Power (kilowatts)", col="Red")
-
 ## Closing the png graphic device
 dev.off()
 
-Consp_Subset$datetime <- ymd_hms(paste(Consp_Subset$Date, Consp_Subset$Time))
-
-png("./ExploratoryDA/plot2.png", width=480,height=480)
-plot(Consp_Subset$datetime,as.numeric(Consp_Subset$Global_active_power), xlab="", ylab="Global Active Power (kilowatts)", type="l")
-dev.off()
-
-sub_metering1 <- as.numeric(Consp_Subset$sub_metering_1)
-sub_metering2 <- as.numeric(Consp_Subset$sub_metering_2)
-sub_metering3 <- as.numeric(Consp_Subset$sub_metering_3)
-
-png("./ExploratoryDA/plot3.png", width=480,height=480)
-plot(Consp_Subset$datetime,as.numeric(Consp_Subset$Global_active_power), xlab="", ylab="Global Active Power (kilowatts)", type="l")
-dev.off()
